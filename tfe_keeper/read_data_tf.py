a0b1146ee3019883bd652f1646c21ab7c0ad974c
境堺 <qizhi.zqz@alibaba-inc.com>
2020-04-16 17:46:22 +0800

    new line

--- tfe_keeper/read_data_tf.py
+++ tfe_keeper/read_data_tf.py
@@ -11,18 +11,18 @@ import tensorflow as tf
 
 def get_data_xy(batch_size, data_file, featureNum, matchColNum=2, epoch=100, clip_by_value=3.0, skip_row_num=1):
     def line_split(r):
-        return tf.decode_csv(r, [["a"]] * matchColNum + [[0.2]] * featureNum + [[1]] , field_delim=", ")
+        return tf.decode_csv(r, [["a"]] * matchColNum + [[0.2]] * featureNum + [[1]], field_delim=", ")
     def norm(x):
         x = tf.cast(x, tf.float32)
         return tf.clip_by_value(x, -clip_by_value, clip_by_value)
 
-    data = tf.data.TextLineDataset(data_file).skip(skip_row_num).map(
-            line_split)  # .shuffle(buffer_size=50000, seed=10086)
+    data = tf.data.TextLineDataset(data_file).skip(skip_row_num).map(line_split)
+    # .shuffle(buffer_size=50000, seed=10086)
 
-    batch_data_iter_x = data.map(lambda *r: tf.stack(r[matchColNum:-1], axis=-1)).map(norm).repeat(epoch).batch(
-        batch_size).make_one_shot_iterator()
-    batch_data_iter_y = data.map(lambda *r: r[-1]).repeat(epoch).batch(
-        batch_size).make_one_shot_iterator()
+    batch_data_iter_x = data.map(lambda *r: tf.stack(r[matchColNum:-1], axis=-1)).map(norm)\
+        .repeat(epoch).batch(batch_size).make_one_shot_iterator()
+    batch_data_iter_y = data.map(lambda *r: r[-1]).repeat(epoch).\
+        batch(batch_size).make_one_shot_iterator()
 
     batch_data_x = batch_data_iter_x.get_next()
     batch_data_x = tf.reshape(batch_data_x, shape=[batch_size, featureNum])
@@ -59,7 +59,7 @@ def get_data_id_with_y(batch_size, data_y_file, matchColNum=2, epoch=100, skip_r
 
 def get_data_id_with_xy(batch_size, data_file, featureNum, matchColNum=2, epoch=100, clip_by_value=3.0, skip_row_num=1):
     def line_split(r):
-        return tf.decode_csv(r, [["a"]] * matchColNum + [[0.2]] * featureNum + [[1]] , field_delim=", ")
+        return tf.decode_csv(r, [["a"]] * matchColNum + [[0.2]] * featureNum + [[1]], field_delim=", ")
     def norm(x):
         x = tf.cast(x, tf.float32)
         return tf.clip_by_value(x, -clip_by_value, clip_by_value)
@@ -93,11 +93,11 @@ def get_data_x(batch_size, data_x_file, featureNum, matchColNum=2, epoch=100, cl
         x = tf.cast(x, tf.float32)
         return tf.clip_by_value(x, -clip_by_value, clip_by_value)
 
-    data = tf.data.TextLineDataset(data_x_file).skip(skip_row_num).map(
-            line_split)  # .shuffle(buffer_size=50000, seed=10086)
+    data = tf.data.TextLineDataset(data_x_file).skip(skip_row_num).map(line_split)
+    # .shuffle(buffer_size=50000, seed=10086)
 
-    batch_data_iter = data.map(lambda *r: tf.stack(r[matchColNum:], axis=-1)).map(norm).repeat(epoch).batch(
-            batch_size).make_one_shot_iterator()
+    batch_data_iter = data.map(lambda *r: tf.stack(r[matchColNum:], axis=-1)).map(norm)\
+        .repeat(epoch).batch(batch_size).make_one_shot_iterator()
 
     batch_data = batch_data_iter.get_next()
     return tf.reshape(batch_data, shape=[batch_size, featureNum])
@@ -108,11 +108,11 @@ def get_data_y(batch_size, data_y_file, matchColNum=2, epoch=100, skip_row_num=1
     def line_split(r):
         return tf.decode_csv(r, [["a"]] * matchColNum + [[0.9]], field_delim=", ")
 
-    data = tf.data.TextLineDataset(data_y_file).skip(skip_row_num).map(
-        line_split)  # .shuffle(buffer_size=50000, seed=10086)
+    data = tf.data.TextLineDataset(data_y_file).skip(skip_row_num)\
+        .map(line_split)  # .shuffle(buffer_size=50000, seed=10086)
 
-    batch_data_iter = data.map(lambda *r: r[matchColNum]).repeat(epoch).batch(
-        batch_size)# .make_one_shot_iterator()
+    batch_data_iter = data.map(lambda *r: r[matchColNum]).repeat(epoch)\
+        .batch(batch_size)# .make_one_shot_iterator()
 
     print("batch_data_iter:", batch_data_iter)
 
