b8609a7e8deea5109ecf38c1d2020505bcdb90b6
境堺 <qizhi.zqz@alibaba-inc.com>
2020-06-10 20:52:05 +0800

    relu_vs_stf

---
+++ vs_stf/relu_vs_stf.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+   Alipay.com Inc.
+   Copyright (c) 2004-2020 All Rights Reserved.
+   ------------------------------------------------------
+   File Name : relu_vs_stf
+   Author : qizhi.zqz
+   Email: qizhi.zqz@alibaba-inc.com
+   Create Time : 2020-06-10 17:34
+   Description : description what the main function of this file
+"""
